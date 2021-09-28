6729a446a6c539fc79ad2bf9350b1589d94b472b
SayMouz <faouz5307@gmail.com>
2021-09-28 16:34:45 -0400

    commentaires ShaderClass

--- ShaderClass.cpp
+++ ShaderClass.cpp
@@ -1,6 +1,5 @@
 #include"shaderClass.h"
 
-// Reads a text file and outputs a string with everything in the text file
 std::string get_file_contents(const char* filename)
 {
 	std::ifstream in(filename, std::ios::binary);
@@ -17,7 +16,6 @@ std::string get_file_contents(const char* filename)
 	throw(errno);
 }
 
-// Constructor that build the Shader Program from 2 different shaders
 Shader::Shader(const char* vertexFile, const char* fragmentFile)
 {
 	// Read vertexFile and fragmentFile and store the strings
@@ -30,20 +28,17 @@ Shader::Shader(const char* vertexFile, const char* fragmentFile)
 
 	// Create Vertex Shader Object and get its reference
 	GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
-	// Attach Vertex Shader source to the Vertex Shader Object
+	//Attach Vertex Shader source to the Vertex Shader Object (shader, number_of_string, string_array, array of string lengths)
 	glShaderSource(vertexShader, 1, &vertexSource, NULL);
 	// Compile the Vertex Shader into machine code
 	glCompileShader(vertexShader);
 	// Checks if Shader compiled succesfully
 	compileErrors(vertexShader, "VERTEX");
 
-	// Create Fragment Shader Object and get its reference
+	// Do exactly the same thing but with the Fragment Shader Object 
 	GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-	// Attach Fragment Shader source to the Fragment Shader Object
 	glShaderSource(fragmentShader, 1, &fragmentSource, NULL);
-	// Compile the Vertex Shader into machine code
 	glCompileShader(fragmentShader);
-	// Checks if Shader compiled succesfully
 	compileErrors(fragmentShader, "FRAGMENT");
 
 	// Create Shader Program Object and get its reference
@@ -59,22 +54,19 @@ Shader::Shader(const char* vertexFile, const char* fragmentFile)
 	// Delete the now useless Vertex and Fragment Shader objects
 	glDeleteShader(vertexShader);
 	glDeleteShader(fragmentShader);
-
 }
 
-// Activates the Shader Program
+
 void Shader::Activate()
 {
 	glUseProgram(ID);
 }
 
-// Deletes the Shader Program
 void Shader::Delete()
 {
 	glDeleteProgram(ID);
 }
 
-// Checks if the different Shaders have compiled properly
 void Shader::compileErrors(unsigned int shader, const char* type)
 {
 	// Stores status of compilation
--- ShaderClass.h
+++ ShaderClass.h
@@ -8,26 +8,48 @@
 #include<iostream>
 #include<cerrno>
 
+/// <summary>
+///	Reads a text file and outputs a string with everything in the text file.
+/// </summary>
+/// <param name="filename">const char* : name or path of the file.</param>
+/// <returns>std::string : content of the text file</returns>
 std::string get_file_contents(const char* filename);
 
+/// <summary>
+/// Handles an GL Shader program with a vertex shader and a fragment shader.
+/// The vertex shader links vertices to draw the object.
+/// The fragment shader handles the color of the object.
+/// </summary>
 class Shader
 {
 public:
 	// Reference ID of the Shader Program
 	GLuint ID;
-	// Constructor that build the Shader Program from 2 different shaders
+	
+	/// <summary>
+	/// Constructor that builds a shader program with two files.
+	/// </summary>
+	/// <param name="vertexFile">const char* : path to the vertex shader code file</param>
+	/// <param name="fragmentFile">const char* : path to the fragment shader code file</param>
 	Shader(const char* vertexFile, const char* fragmentFile);
 
-	// Default construct (unusable, just here to avoid errors)
-	Shader() { ID = 0; }
-
-	// Activates the Shader Program
+	/// <summary>
+	/// Activates the Shader Program, makes GL use it.
+	/// </summary>
 	void Activate();
-	// Deletes the Shader Program
+
+	/// <summary>
+	/// Deletes the Shader Program
+	/// </summary>
 	void Delete();
 
 private:
-	// Checks if the different Shaders have compiled properly
+	
+	/// <summary>
+	/// Checks if the different Shaders have compiled properly
+	/// </summary>
+	/// <param name="shader">unsigned int : ID of the shader to check</param>
+	/// <param name="type">const char* : type of the shader</param>
 	void compileErrors(unsigned int shader, const char* type);
 };
 #endif
