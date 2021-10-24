c9ed4b5240be137285afd90fc1f36ad024b02004
Labbe Max <max.labbe.m@gmail.com>
2021-10-24 15:08:08 -0400

    generateur de gravité

--- Engine/include/GravityGenerator.h
+++ Engine/include/GravityGenerator.h
@@ -2,12 +2,19 @@
 #ifndef GRAVITYGENERATOR_H
 #define GRAVITYGENERATOR_H
 
+#include<Common.h>
 #include<ParticleForceGenerator.h>
+
 
 class GravityGenerator : IParticleForceGenerator
 {
 public: 
+	GravityGenerator();
+	GravityGenerator(float gravityValue);
 	void updateForce(Particle* particle, float duration);
+
+private:
+	Vector3D m_gravityValues;
 };
 
 #endif
--- Engine/src/GravityGenerator.cpp
+++ Engine/src/GravityGenerator.cpp
@@ -1,6 +1,13 @@
 #include <GravityGenerator.h>
 
+GravityGenerator::GravityGenerator() : m_gravityValues(Vector3D(0, -9.81f, 0)) {}
+GravityGenerator::GravityGenerator(float gravityValue) : m_gravityValues(Vector3D(0, -gravityValue, 0)) {}
+
 void GravityGenerator::updateForce(Particle* particle, float duration)
 {
-
+	float mass = particle->getMass();
+	if (mass != 0)
+	{
+		particle->addForce(m_gravityValues.multiplyByScalar(mass));
+	}
 }
