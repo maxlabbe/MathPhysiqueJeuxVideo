7a29b4c0b52d664c7179f3fc069a777beaf68916
Labbe Max <max.labbe.m@gmail.com>
2021-10-25 18:23:03 -0400

    voila

--- Engine/src/ParticleContact.cpp
+++ Engine/src/ParticleContact.cpp
@@ -3,6 +3,7 @@
 ParticleContact::ParticleContact(Particle* particles[2])
 	: m_particles(), m_restitutionCoef(0), m_contactPointNormal(Vector3D()), m_penetration(Vector3D()), m_impulsion(Vector3D())
 {
+	// Add the particles in the particles array
 	m_particles[0] = particles[0];
 	m_particles[1] = particles[1];
 }
@@ -11,13 +12,20 @@ ParticleContact::ParticleContact(Particle* particles[2])
 ParticleContact::ParticleContact(Particle* particles[2], float restitutionCoef)
 	: m_particles(), m_restitutionCoef(restitutionCoef), m_contactPointNormal(Vector3D()), m_penetration(Vector3D()), m_impulsion(Vector3D()) 
 {
+	// Add the particles in the particles array
 	m_particles[0] = particles[0];
 	m_particles[1] = particles[1];
 }
 
-void ParticleContact::resolve(float duration) {}
+void ParticleContact::resolve(float duration) 
+{
+	
+}
 
-void ParticleContact::computeApproachVelocity() const {}
+void ParticleContact::computeApproachVelocity() const 
+{
+
+}
 
 void ParticleContact::ResolveImpulsion(float duration) {}
 
