ef7bd71e84f2f1e604552310fce810dfd2d39fd0
Labbe Max <max.labbe.m@gmail.com>
2021-10-24 20:55:22 -0400

    ajout du premier ressort et corrections

--- Engine/src/DragGenerator.cpp
+++ Engine/src/DragGenerator.cpp
@@ -6,36 +6,11 @@ DragGenerator::DragGenerator(float k1, float k2) : m_k1(k1), m_k2(k2) {}
 
 void DragGenerator::updateForce(Particle* particle, float duration)
 {
-	// We need a unit vector in the same direction as the velocity
-	// unit vector u, velocity vector v
-	// u(x, y, z) = v(x/|x|, y/|y|, z/|z|) 
-	Vector3D velocity = particle->getVelocity();
-	float x = velocity.getX();
-	float y = velocity.getY();
-	float z = velocity.getZ();
+	float velocityNorm = particle->getVelocity().norm();
 
-	// if x, y or z equal 0 we can't divide it
-	if (x != 0)
-	{
-		x = x / abs(x);
-	}
-
-	if (y != 0)
-	{
-		y = y / abs(y);
-	}
-
-	if (z != 0)
-	{
-		z = z / abs(z);
-	}
-	Vector3D velocityUnitVector = Vector3D(x, y, z);
-
-	// drag coef = k1 * ||v|| + k2 * ||v||²
-	float velocityNorm = velocity.norm();
-	float dragValue = m_k1 * velocityNorm + m_k2 * velocityNorm * velocityNorm;
-
-	//add the force to the particle
-	particle->addForce(velocityUnitVector * dragValue);
+	// We need a unit vector in the same direction as the velocity to compute the force
+	// Drag coef = k1 * ||v|| + k2 * ||v||²
+	// Add the force to the particle
+	particle->addForce(particle->getVelocity().normalize() * (m_k1 * velocityNorm + m_k2 * velocityNorm * velocityNorm));
 	
 }
