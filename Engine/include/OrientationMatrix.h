70c2922ce3d7a838e14e8d477ce5a8cbe656bdaa
Labbe Max <max.labbe.m@gmail.com>
2021-11-15 11:34:58 -0500

    matrice orientation

---
+++ Engine/include/OrientationMatrix.h
@@ -0,0 +1,16 @@
+#pragma once
+#include<array>
+#include <Vector3D.h>
+class OrientationMatrix : public Vector3D
+{
+	//tab[]i[j] i = ligne j = colonne
+public:
+	OrientationMatrix();
+	OrientationMatrix(float p1, float q1, float r1, float p2, float q2, float r2);
+	~OrientationMatrix();
+	const array<array<float, 3>, 3> GetMatrix();
+
+private:
+	array<array<float, 3>, 3> m_matrix;
+};
+
---
+++ Engine/src/OrientationMatrix.cpp
@@ -0,0 +1,26 @@
+#include <OrientationMatrix.h>
+
+OrientationMatrix::OrientationMatrix()
+	: OrientationMatrix(0, 0, 0, 0, 0, 0) {}
+
+OrientationMatrix::OrientationMatrix(float p1, float q1, float r1, float p2, float q2, float r2)
+{
+	m_matrix[0][0] = p1 * p2;
+	m_matrix[0][1] = q1 * p2;
+	m_matrix[0][2] = r1 * p2;
+
+	m_matrix[1][0] = p1 * q2;
+	m_matrix[1][1] = q1 * q2;
+	m_matrix[1][2] = r1 * q2;
+
+	m_matrix[2][0] = p1 * r2;
+	m_matrix[2][1] = q1 * r2;
+	m_matrix[2][2] = r1 * r2;
+}
+
+OrientationMatrix::~OrientationMatrix() {}
+
+const array<array<float, 3>, 3> OrientationMatrix::GetMatrix()
+{
+	return m_matrix;
+}
