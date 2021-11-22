2601dca29d71de5d2dbb484d30500f2eed1ceac7
patate FAYOT <36509487+patateFAYOT@users.noreply.github.com>
2021-11-22 13:42:02 -0500

    Update Matrix3.h

--- Engine/include/MathTools/Matrix3.h
+++ Engine/include/MathTools/Matrix3.h
@@ -16,14 +16,56 @@ public:
 	// Setter
 	void setMatrix(array<array<float, 3>, 3> matrix) { m_matrix = matrix; }
 
+	/// <summary>
+	/// multiply the matrix by a scalar
+	/// </summary>
+	/// <param name="c"> float : the scalar that multiply the matrix </param>
+	/// <returns> Matrix3 : the new scaled matrix </returns>
 	Matrix3 multiplyByScalar(const float& c);
+	
+	/// <summary>
+	/// multiply the matrix by a given vector
+	/// </summary>
+	/// <param name="vector"> Vector3D : the vector that multiply the matrix </param>
+	/// <returns> Vector3D : the new matrix </returns>
 	Vector3D multiplyByVector(Vector3D vector);
+	
+	/// <summary>
+	/// multiply the matrix by an other matrix
+	/// </summary>
+	/// <param name="matrix"> float : the other matrix that multiply the matrix </param>
+	/// <returns> Matrix3 : the new matrix </returns>
 	Matrix3 multiplyByMatrix(Matrix3 matrix);
+	
+	/// <summary>
+	/// compute the inverse of the matrix
+	/// </summary>
+	/// <returns> Matrix3 : the inversed matrix </returns>
 	Matrix3 inverse();
+	
+	/// <summary>
+	/// compute the transpose of the matrix
+	/// </summary>
+	/// <returns> Matrix3 : the transposed matrix </returns>
 	Matrix3 transpose();
+	
+	/// <summary>
+	/// compute the determinant of the matrix
+	/// </summary>
+	/// <returns> float : the determinant of the matrix </returns>
 	float determinant();
 
+	/// <summary>
+	/// Ctor
+	/// Create the identity matrix
+	/// </summary>
 	Matrix3();
+	
+	/// <summary>
+	/// Ctor
+	/// Create a personnalize matrix
+	/// </summary>
+	/// <param name="matrix"> the array of the matrix </param>
 	Matrix3(array<array<float, 3>, 3> matrix);
 
 protected:
