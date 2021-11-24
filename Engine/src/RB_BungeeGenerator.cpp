1633613d9469f176869731235048ae03e19dd74a
Claire <Claire@JOSEPH>
2021-11-24 00:29:23 -0500

    Mise à jour des commentaires

--- Engine/src/RB_BungeeGenerator.cpp
+++ Engine/src/RB_BungeeGenerator.cpp
@@ -2,11 +2,16 @@
 
 void RB_BungeeGenerator::updateForce(RigidBody* rigidBody, Vector3D* apllicationPoint, float duration)
 {
-	// l is the spring stretching so the distance beetween the two particles
+	// l is the spring stretching so the distance beetween the two application points
 	Vector3D distance = *apllicationPoint - m_applicationPoint;
 
+	// The elastic do something only when it's tense
+	//That mean the distance beetween the application points are bigger than the elastic's rest size
 	if (distance.norm() > m_l0)
 	{
+		// Now we compute the value of the force add it to the particle
+		// To compute the force we calculate the force's value and apply it to the distance's unit vector
+		//F = k(l0 - l)
 		rigidBody->addForceAtPoint(distance.normalize() * (m_k * (m_l0 - distance.norm())), *apllicationPoint);
 	}
 }
--- Engine/src/RB_Buoyancy.cpp
+++ Engine/src/RB_Buoyancy.cpp
@@ -1,6 +1,6 @@
 #include <RB_Buoyancy.h>
 
-// The default Ctor is for 10m of water for an object of 1m³
+// The default Ctor is for 10m of water
 RB_Buoyancy::RB_Buoyancy() : RB_Buoyancy(2.0f, 10.0f, 1000.0f) {}
 
 RB_Buoyancy::RB_Buoyancy(float maxDepth, float liquidHeight, float density)
@@ -8,18 +8,17 @@ RB_Buoyancy::RB_Buoyancy(float maxDepth, float liquidHeight, float density)
 
 void RB_Buoyancy::updateForce(RigidBody* rigidBody, float duration, Vector3D* applicationPoint)
 {
-	// The particle's depth is it's y coordinate
 	float rigidBodyHeight = rigidBody->GetHeight();
 	float rigidBodyVolume = rigidBody->GetVolume();
 
-	// If the particle is completely submerged
+	// If the rigid body is completely submerged
 	// F = objectVolume * liquidDensity
 	if (rigidBodyHeight <= m_maxDepth - m_liquidHeight)
 	{
 		rigidBody->addForceAtPoint(Vector3D(0, rigidBodyVolume * m_density, 0), rigidBody->GetMassCenter());
 	}
 
-	// If the particle is part submerged
+	// If the rigid body is part submerged
 	// F = object submerged depth * objectVolume * liquidDensity
 	else if (rigidBodyHeight >= m_maxDepth - m_liquidHeight && rigidBodyHeight <= m_maxDepth + m_liquidHeight)
 	{
